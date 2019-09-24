660ed7077d9f05c5b1814d4f6ae471bab9130c44
Shehan Jiffry <dsjiffry@gmail.com>
2019-09-24 11:06:16 +0530

    Started connecting backend and fontend
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

--- backend/Ticketing/src/main/java/com/example/demo/Main.java
+++ backend/Ticketing/src/main/java/com/example/demo/Main.java
@@ -12,9 +12,6 @@ public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 		
-		LocalPassenger lp = new LocalPassenger("John", "345345", "4564345", 54354, "test", new Date());
-		lp.addUser();
-		
 	}
 	
 
