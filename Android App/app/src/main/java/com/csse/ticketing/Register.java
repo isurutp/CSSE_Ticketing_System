491b41e45ebd287235344879da02d3e345a9cf70
Shehan Jiffry <dsjiffry@gmail.com>
2019-09-29 09:54:24 +0530

    Added main screen
    Signed-off-by: Shehan Jiffry <dsjiffry@gmail.com>

--- Android App/app/src/main/java/com/csse/ticketing/Register.java
+++ Android App/app/src/main/java/com/csse/ticketing/Register.java
@@ -2,7 +2,9 @@ package com.csse.ticketing;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void LoginButton(View view)
+    {
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
